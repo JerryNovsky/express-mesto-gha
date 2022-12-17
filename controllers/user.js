527a0a95c75574857e294b723fcfe7b392f6798a
Aleksandr Maksimov <lucky_tower@mail.ru>
2022-12-17 15:43:26 +0700

    13pr fix: Устранены ошибки автотестов

--- controllers/user.js
+++ controllers/user.js
@@ -56,7 +56,11 @@ module.exports.createUser = (req, res) => {
 
 module.exports.updateUserInfo = (req, res) => {
   const { name, about } = req.body;
-  User.findByIdAndUpdate(req.user._id, { name, about }, { new: true })
+  User.findByIdAndUpdate(
+    req.user._id,
+    { name, about },
+    { new: true, runValidators: true }
+  )
     .then((user) => {
       if (!user) {
         return res
@@ -79,7 +83,7 @@ module.exports.updateUserInfo = (req, res) => {
 
 module.exports.updateUserAvatar = (req, res) => {
   const { avatar } = req.body;
-  User.findByIdAndUpdate(req.user._id, { avatar })
+  User.findByIdAndUpdate(req.user._id, { avatar }, { new: true })
     .then((user) => {
       if (!user) {
         return res

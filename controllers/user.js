54580955f28601be34d0cb2f33eb12c0c1972d53
Aleksandr Maksimov <lucky_tower@mail.ru>
2022-12-18 13:55:01 +0700

    13pr fix: Устранены ошибки автотестов

--- controllers/user.js
+++ controllers/user.js
@@ -26,8 +26,8 @@ module.exports.getUserById = (req, res) => {
       if (err.message === 'NotValidId') {
         res.status(NOT_FOUND).send({ message: `ERROR ${NOT_FOUND}: User not found` });
       } else {
-        res.status(INTERNAL_SERVER_ERROR)
-          .send({ message: `ERROR ${INTERNAL_SERVER_ERROR}: Server error` });
+        res.status(BAD_REQUEST)
+          .send({ message: `ERROR ${BAD_REQUEST}: Server error` });
       }
     });
 };
